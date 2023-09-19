5baa1e872b5511b180793b6e6408938c67ce29d2
Reza Moradi <rezamoradix@gmail.com>
2023-09-19 19:48:52 +0330

    Create EasyEntity.php

---
+++ src/EasyEntity.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Rey\Easy;
+
+use ReflectionClass;
+
+/**
+ * EasyEntity
+ */
+trait EasyEntity
+{
+    public function hasOne($className, $key = null)
+    {
+        $name = (new ReflectionClass($className))->getShortName();
+        $key ??= $name . '_id';
+        if (!isset($this->attributes[$name])) $this->attributes[$name] = $className::i()->where($key, $this->id)->first();
+        return $this->attributes[$name];
+    }
+
+    public function belongsTo($className, $key = null)
+    {
+        $name = (new ReflectionClass($className))->getShortName();
+        $key ??= $name . '_id';
+        if (!isset($this->attributes[$name])) $this->attributes[$name] = $className::fetch($this->$key);
+        return $this->attributes[$name];
+    }
+}
